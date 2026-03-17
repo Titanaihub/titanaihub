0f6537ce51225e5e533798caf831c214a5e4b624
Titanaihub <32965272+Titanaihub@users.noreply.github.com>
2026-03-18 02:15:48 +0700

    Update btc.js

--- api/btc.js
+++ api/btc.js
@@ -1,17 +1,18 @@
 export default async function handler(req, res) {
   try {
-    const r = await fetch("https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT");
+    const r = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
     const data = await r.json();
 
     res.status(200).json({
       ok: true,
-      price: Number(data.lastPrice),
-      volume: Number(data.volume)
+      price: parseFloat(data.price),
+      volume: 0
     });
+
   } catch (e) {
     res.status(500).json({
       ok: false,
-      error: "failed_to_fetch_btc"
+      error: e.toString()
     });
   }
 }
