e7d9c44a54a6bd1a8c6be15cd3a319e21bbe1878
Titanaihub <32965272+Titanaihub@users.noreply.github.com>
2026-03-18 11:50:16 +0700

    Update bnb.js

--- api/bnb.js
+++ api/bnb.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+  try {
+    const r = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT");
+    const data = await r.json();
 
+    res.status(200).json({
+      ok: true,
+      symbol: "BNB",
+      price: Number(data.price)
+    });
+
+  } catch (e) {
+    res.status(500).json({
+      ok: false,
+      error: e.toString()
+    });
+  }
+}
