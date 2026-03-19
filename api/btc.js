f345414fe6804264949dea85fbd43eb8b15b9abc
Titanaihub <32965272+Titanaihub@users.noreply.github.com>
2026-03-19 11:40:21 +0700

    Update btc.js

--- api/btc.js
+++ api/btc.js
@@ -1,17 +1,1 @@
-export default async function handler(req, res) {
-  try {
-    const r = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
-    const data = await r.json();
 
-    res.status(200).json({
-      ok: true,
-      price: Number(data.bitcoin.usd),
-      volume: 0
-    });
-  } catch (e) {
-    res.status(500).json({
-      ok: false,
-      error: e.message
-    });
-  }
-}
