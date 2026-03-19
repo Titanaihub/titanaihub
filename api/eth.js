68a2b45bf68d7afbb3649d432d8ea32fd4a5e76e
Titanaihub <32965272+Titanaihub@users.noreply.github.com>
2026-03-19 12:21:18 +0700

    Update eth.js

--- api/eth.js
+++ api/eth.js
@@ -1,18 +1,1 @@
-export default async function handler(req, res) {
-  try {
-    const r = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT");
-    const data = await r.json();
 
-    res.status(200).json({
-      ok: true,
-      symbol: "ETH",
-      price: Number(data.price)
-    });
-
-  } catch (e) {
-    res.status(500).json({
-      ok: false,
-      error: e.toString()
-    });
-  }
-}
