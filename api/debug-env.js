2d893060adb8f61bb539c3692a10d5e79f9f7286
Titanaihub <32965272+Titanaihub@users.noreply.github.com>
2026-03-19 20:22:18 +0700

    Update debug-env.js

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,1 +1,10 @@
+module.exports = async (req, res) => {
+  const key = process.env.COINGECKO_API_KEY || "";
 
+  res.status(200).json({
+    ok: true,
+    exists: !!key,
+    prefix: key ? key.slice(0, 6) : null,
+    length: key ? key.length : 0
+  });
+};
