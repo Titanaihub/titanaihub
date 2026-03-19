bd1e8edcbee26a32fd2816be780b4b1ae31eee99
Titanaihub <32965272+Titanaihub@users.noreply.github.com>
2026-03-19 20:42:22 +0700

    Update market-data-btc.js

--- api/market-data-btc.js
+++ api/market-data-btc.js
@@ -1,16 +1,1 @@
-const { fetchCoinMarketData } = require("../lib/marketDataService");
 
-module.exports = async (req, res) => {
-  try {
-    const data = await fetchCoinMarketData("bitcoin", "BTC");
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({
-      ok: false,
-      symbol: "BTC",
-      coinId: "bitcoin",
-      source: "coingecko",
-      error: err.toString()
-    });
-  }
-};
